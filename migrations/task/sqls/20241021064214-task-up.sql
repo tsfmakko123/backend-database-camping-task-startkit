edd60c2589bc1fa5edb0d54a0bfe02d72eec0db9
tsfmakko123 <acerfp563@msn.com>
2024-12-06 03:15:40 +0800

    Update 20241021064214-task-up.sql

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -330,18 +330,10 @@ group by "CREDIT_PACKAGE".name
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
 SELECT 
-   SUM("CREDIT_PACKAGE".price) AS "總營收"
-FROM 
-   "CREDIT_PURCHASE"
-INNER JOIN 
-   "CREDIT_PACKAGE" 
-ON 
-   "CREDIT_PACKAGE".id = "CREDIT_PURCHASE".credit_package_id
-WHERE 
-   EXTRACT(MONTH FROM "CREDIT_PURCHASE".purchase_at) = 11
-   AND EXTRACT(YEAR FROM "CREDIT_PURCHASE".purchase_at) = 2024
-   AND status <> '課程已取消'
-;
+  COUNT(DISTINCT("COURSE_BOOKING".user_id)) AS "預約會員人數"
+FROM "COURSE_BOOKING"
+WHERE "COURSE_BOOKING".created_at >= '2024-11-01 00:00:00' AND "COURSE_BOOKING".created_at <= '2024-11-30 23:59:59'
+AND status != '課程已取消';
 
 
 
