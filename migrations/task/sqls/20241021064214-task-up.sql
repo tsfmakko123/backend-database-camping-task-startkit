f3d845d52576f518fa85adca9b8897a3b7e4f5f6
tsfmakko123 <acerfp563@msn.com>
2024-12-07 15:23:13 +0800

    Update 20241021064214-task-up.sql

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -371,16 +371,13 @@ group by
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
-
-SELECT
-	SUM(price_paid) AS 總營收
-FROM
-	"CREDIT_PURCHASE"
-WHERE
-	purchase_at >= '2024-11-01 00:00:00'
-	AND
-	purchase_at <= '2024-11-30 23:59:59';
-
+SELECT 
+   sum(price_paid) as "總營收"
+FROM 
+    "CREDIT_PURCHASE"
+WHERE 
+    EXTRACT(MONTH FROM "CREDIT_PURCHASE".purchase_at) = 11
+;
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
 
