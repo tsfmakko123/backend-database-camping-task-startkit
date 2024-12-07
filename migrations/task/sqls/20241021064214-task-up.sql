374dc5b6ed87cd6dcb9cc37cb6ab894b80528285
tsfmakko123 <acerfp563@msn.com>
2024-12-07 15:39:03 +0800

    Update 20241021064214-task-up.sql

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -157,8 +157,11 @@ WHERE user_id IN (
 );
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
-INSERT INTO "SKILL" (name) VALUES ('空中瑜伽');
-DELETE FROM "SKILL" WHERE name = '空中瑜伽';
+insert into "SKILL"(name)
+values('空中瑜伽');
+
+delete from "SKILL" 
+where name='空中瑜伽';
 
 
 
